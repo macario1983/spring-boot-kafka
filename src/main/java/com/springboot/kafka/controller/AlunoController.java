da0f47b38be79c5de4886512e6fb6e965738651e
Diego Macario <diego_macario@hotmail.com>
2023-08-25 17:44:56 -0300

    Ajustando o projeto para receber mais requisções

--- src/main/java/com/springboot/kafka/controller/AlunoController.java
+++ src/main/java/com/springboot/kafka/controller/AlunoController.java
@@ -4,6 +4,7 @@ import com.springboot.kafka.dto.AlunoDTO;
 import com.springboot.kafka.service.AlunoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,7 +22,7 @@ public class AlunoController {
 
     @PostMapping
     public ResponseEntity<AlunoDTO> insert(@RequestBody @Valid AlunoDTO alunoDTO) {
-        return ResponseEntity.ok().body(alunoService.send(alunoDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoService.send(alunoDTO));
     }
 
 }
